346f98d6d31b751458cdc3ca81900f0dac262ef1
wiki3007 <wiki3007.ww@gmail.com>
2021-05-31 17:25:18 +0200

    Commit serwera do obsługi gry

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+let path = require("path")
+let bodyParser = require("body-parser")
+let PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/index.html"));
+})
+
+app.post("/game", function (req, res) {
+    res.status(200).send(req.body.nick + ", tu wstawić właściwą grę z obsługą fetcha i NeDB.");
+})
+
+app.listen(PORT, function () {
+    console.log("Serwer działa na porcie " + PORT)
+})
